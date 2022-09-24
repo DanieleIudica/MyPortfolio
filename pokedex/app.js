f401c0a5a97518f099c7447ab6469943d330e9e6
Daniele Iudica <102958741+DanieleIudica@users.noreply.github.com>
2022-09-24 15:46:13 +0200

    add_linkedin_clone

--- pokedex/app.js
+++ pokedex/app.js
@@ -23,7 +23,26 @@ const rightButton = document.querySelector(".right-button");
 // console.log(pokeListItems); // prendo tutti i div con querySelectorAll
 
 // array dei pokemon types che corrispondono alle classi css
-const TYPES = ["normal", "fighting", "flying", "poison", "ground", "rock", "bug", "ghost", "steel", "fire", "water", "grass", "electric", "psychic", "ice", "dragon", "dark", "fairy"];
+const TYPES = [
+    "normal",
+    "fighting",
+    "flying",
+    "poison",
+    "ground",
+    "rock",
+    "bug",
+    "ghost",
+    "steel",
+    "fire",
+    "water",
+    "grass",
+    "electric",
+    "psychic",
+    "ice",
+    "dragon",
+    "dark",
+    "fairy",
+];
 
 // previous url & next url
 let prevUrl = null;
@@ -36,103 +55,103 @@ const capitalize = (str) => str[0].toUpperCase() + str.substr(1);
 
 // funzione per resettare il colore dello screen
 const resetScreen = () => {
-  mainScreen.classList.remove("hide"); //accendo lo screen
-  // for of dell array per iterare tutte le classi/type
-  for (const i of TYPES) {
-    //   ed eliminare qualunqhe sia la classe del poke visualizzato in precedenza
-    mainScreen.classList.remove(i);
-    // console.log(i);
-  }
+    mainScreen.classList.remove("hide"); //accendo lo screen
+    // for of dell array per iterare tutte le classi/type
+    for (const i of TYPES) {
+        //   ed eliminare qualunqhe sia la classe del poke visualizzato in precedenza
+        mainScreen.classList.remove(i);
+        // console.log(i);
+    }
 };
 
 //! fetch che recupera dati per il lato destro dello schermo
 //   gli passo url come parametro
 const fetchPokeList = (url) => {
-  fetch(url) // al posto che avere un url fissa, gli passo un url variabile tramite parametro. per navigare con prev e next
-    .then((response) => response.json())
-    .then((data) => {
-      // console.log(data);
-      //   mi restituisce un array con i primi 20 poke
-      // const results = data["results"]; // quando vuoi dare alla variabile lo stesso nome della proprieta' come in questo caso, puoi destrutturizzare in questo modo
-      const { results, previous, next } = data;
-      prevUrl = previous;
-      nextUrl = next;
-      console.log(next);
+    fetch(url) // al posto che avere un url fissa, gli passo un url variabile tramite parametro. per navigare con prev e next
+        .then((response) => response.json())
+        .then((data) => {
+            // console.log(data);
+            //   mi restituisce un array con i primi 20 poke
+            // const results = data["results"]; // quando vuoi dare alla variabile lo stesso nome della proprieta' come in questo caso, puoi destrutturizzare in questo modo
+            const { results, previous, next } = data;
+            prevUrl = previous;
+            nextUrl = next;
+            console.log(next);
 
-      console.log(results);
-      for (let i = 0; i < pokeListItems.length; i++) {
-        const pokeListItem = pokeListItems[i];
-        //   console.log(pokeListItem);
-        const resultData = results[i];
-        //   console.log(resultData);
+            console.log(results);
+            for (let i = 0; i < pokeListItems.length; i++) {
+                const pokeListItem = pokeListItems[i];
+                //   console.log(pokeListItem);
+                const resultData = results[i];
+                //   console.log(resultData);
 
-        // se esiste carica i dati
-        if (resultData) {
-          const { name, url } = resultData;
-          const urlArray = url.split("/"); // creo un arrai splittando gli elementi da '/'
-          //console.log(urlArray[6]); //il sesto elemento e' il numero che ci serve
-          const id = urlArray[6];
-          pokeListItem.textContent = `${id}.  ${capitalize(name)}`;
-          // altrimenti, stringa vuota
-        } else {
-          pokeListItem.textContent = "";
-        }
-      }
-    });
+                // se esiste carica i dati
+                if (resultData) {
+                    const { name, url } = resultData;
+                    const urlArray = url.split("/"); // creo un arrai splittando gli elementi da '/'
+                    //console.log(urlArray[6]); //il sesto elemento e' il numero che ci serve
+                    const id = urlArray[6];
+                    pokeListItem.textContent = `${id}.  ${capitalize(name)}`;
+                    // altrimenti, stringa vuota
+                } else {
+                    pokeListItem.textContent = "";
+                }
+            }
+        });
 };
 
 //! fetch che recupera dati per il lato sinistro dello schermo
 const fetchPokeData = (pokeNum) =>
-  fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      // reset schermo
-      resetScreen();
-      // types
-      const dataTypes = data["types"]; // recupero array del tipo
-      const dataFirstType = dataTypes[0]; // li divido perche non tutti i pokemon hanno 2 tipi
-      const dataSecondType = dataTypes[1]; // bisognera' creare un if
-      pokeTypeOne.textContent = capitalize(dataFirstType["type"]["name"]); // recupero nome primo tipo
-      if (dataSecondType) {
-        pokeTypeTwo.classList.remove("hide"); // se esiste la rimuove
-        pokeTypeTwo.textContent = capitalize(dataSecondType["type"]["name"]); // se esiste recupero nome secondo tipo (if mi da un boolean)
-      } else {
-        pokeTypeTwo.classList.add("hide"); // nascondo lo span nel caso il poke non abbia il secondType
-        pokeTypeTwo.textContent = ""; // svuoto il campo altrimenti andando al poke successivo continuero' a visualizzare il vecchio type
-      }
-      mainScreen.classList.add(dataFirstType["type"]["name"]); // va ad aggiungere il type come classe che corrisponde al bg color associato nel css!
-      pokeName.textContent = capitalize(data["name"]); // recupero delle singole info
-      pokeId.textContent = "#" + data["id"].toString().padStart(3, "0"); // converto in stringa e uso padStart per fare in modo che abbia 3 caratteri, se piu corta verranno rimpiazzati da 0
-      pokeWeight.textContent = data["weight"];
-      pokeHeight.textContent = data["height"];
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            // reset schermo
+            resetScreen();
+            // types
+            const dataTypes = data["types"]; // recupero array del tipo
+            const dataFirstType = dataTypes[0]; // li divido perche non tutti i pokemon hanno 2 tipi
+            const dataSecondType = dataTypes[1]; // bisognera' creare un if
+            pokeTypeOne.textContent = capitalize(dataFirstType["type"]["name"]); // recupero nome primo tipo
+            if (dataSecondType) {
+                pokeTypeTwo.classList.remove("hide"); // se esiste la rimuove
+                pokeTypeTwo.textContent = capitalize(dataSecondType["type"]["name"]); // se esiste recupero nome secondo tipo (if mi da un boolean)
+            } else {
+                pokeTypeTwo.classList.add("hide"); // nascondo lo span nel caso il poke non abbia il secondType
+                pokeTypeTwo.textContent = ""; // svuoto il campo altrimenti andando al poke successivo continuero' a visualizzare il vecchio type
+            }
+            mainScreen.classList.add(dataFirstType["type"]["name"]); // va ad aggiungere il type come classe che corrisponde al bg color associato nel css!
+            pokeName.textContent = capitalize(data["name"]); // recupero delle singole info
+            pokeId.textContent = "#" + data["id"].toString().padStart(3, "0"); // converto in stringa e uso padStart per fare in modo che abbia 3 caratteri, se piu corta verranno rimpiazzati da 0
+            pokeWeight.textContent = data["weight"];
+            pokeHeight.textContent = data["height"];
 
-      // imgs
-      pokeFrontImage.src = data["sprites"]["front_default"] || ""; // || stringa vuota in caso non dovesse trovare l'img
-      pokeBackImage.src = data["sprites"]["back_default"] || "";
-    });
+            // imgs
+            pokeFrontImage.src = data["sprites"]["front_default"] || ""; // || stringa vuota in caso non dovesse trovare l'img
+            pokeBackImage.src = data["sprites"]["back_default"] || "";
+        });
 
 //! bottoni prev & next
 const rightButtonClick = () => {
-  if (nextUrl) {
-    fetchPokeList(nextUrl);
-  }
+    if (nextUrl) {
+        fetchPokeList(nextUrl);
+    }
 };
 
 const leftButtonClick = () => {
-  if (prevUrl) {
-    fetchPokeList(prevUrl);
-  }
+    if (prevUrl) {
+        fetchPokeList(prevUrl);
+    }
 };
 // !bottoni pokeList
 const listItemClick = (e) => {
-  // console.log(e.target.textContent); //seleziona il contenuto del singolo bottone
-  if (!e.target) return; // se il target non esiste, non fare nulla
-  const listItem = e.target;
-  if (!listItem.textContent) return; // se il target contiene una stringa vuota (ultima pagina poke, non fare nulla)
-  const textArray = listItem.textContent.split("."); //creo un array della stringa divisa da '.' in modo da recuperare il numero
-  const pokeNum = textArray[0];
-  fetchPokeData(pokeNum);
+    // console.log(e.target.textContent); //seleziona il contenuto del singolo bottone
+    if (!e.target) return; // se il target non esiste, non fare nulla
+    const listItem = e.target;
+    if (!listItem.textContent) return; // se il target contiene una stringa vuota (ultima pagina poke, non fare nulla)
+    const textArray = listItem.textContent.split("."); //creo un array della stringa divisa da '.' in modo da recuperare il numero
+    const pokeNum = textArray[0];
+    fetchPokeData(pokeNum);
 };
 
 // aggiungo gli event listeners
@@ -140,7 +159,7 @@ leftButton.addEventListener("click", leftButtonClick);
 rightButton.addEventListener("click", rightButtonClick);
 // bottoni pokeList, creazione di un eventlistener per ogni item con un for of
 for (const pokeListItem of pokeListItems) {
-  pokeListItem.addEventListener("click", listItemClick);
+    pokeListItem.addEventListener("click", listItemClick);
 }
 
 // inizializzo lo schermo a destra
--- todoapp/script.js
+++ todoapp/script.js
@@ -4,22 +4,24 @@ const inputButton = document.querySelector("#button");
 const taskList = document.querySelector("#task-list");
 //
 document.addEventListener("DOMContentLoaded", function () {
-  // if (document.location.pathname === "/index.html") {
-  if (document.location.pathname === "danieleiudica.github.io/MyPortfolio/todoapp//index.html") {
-    let jsonTask = localStorage.getItem("localTask");
-    console.log(jsonTask);
-    if (jsonTask !== null) {
-      let prova = JSON.parse(jsonTask); //se il local storage non e' vuoto
-      console.log(prova);
-      taskList.innerHTML = prova; // caricane il contenuto nella pagina
+    // if (document.location.pathname === "/index.html") {
+    if (document.location.pathname === "danieleiudica.github.io/MyPortfolio/todoapp//index.html") {
+        let jsonTask = localStorage.getItem("localTask");
+        console.log(jsonTask);
+        if (jsonTask !== null) {
+            let prova = JSON.parse(jsonTask); //se il local storage non e' vuoto
+            console.log(prova);
+            taskList.innerHTML = prova; // caricane il contenuto nella pagina
+        }
+        inputButton.addEventListener("click", aggiungiTask);
+        taskList.addEventListener("click", rimuoviTask);
+        logout.addEventListener("click", slogga);
+    } else if (
+        document.location.pathname === "danieleiudica.github.io/MyPortfolio/todoapp/login.html"
+    ) {
+        // } else if (document.location.pathname === "/login.html") {
+        login.addEventListener("click", check);
     }
-    inputButton.addEventListener("click", aggiungiTask);
-    taskList.addEventListener("click", rimuoviTask);
-    logout.addEventListener("click", slogga);
-  } else if (document.location.pathname === "danieleiudica.github.io/MyPortfolio/todoapp/login.html") {
-    // } else if (document.location.pathname === "/login.html") {
-    login.addEventListener("click", check);
-  }
 });
 
 // pagina login
@@ -29,50 +31,52 @@ const login = document.querySelector(".login");
 const error = document.querySelector(".error");
 
 function check() {
-  console.log(email.value);
-  if (email.value === "" || email.value === null) {
-    error.innerHTML = `<p>Inserisci una mail.</p>`;
-  } else if (pw.value === "" || pw.value === null) {
-    error.innerHTML = `<p>Inserisci una password.</p>`;
-  } else if (!validateEmail(email.value)) {
-    error.innerHTML = `<p>inserire una mail valida.</p>`;
-  } else if (pw.value.length < 8) {
-    error.innerHTML = `<p>La password deve avere almeno 8 caratteri.</p>`;
-  } else {
-    login.setAttribute("href", "index.html");
-  }
+    console.log(email.value);
+    if (email.value === "" || email.value === null) {
+        error.innerHTML = `<p>Inserisci una mail.</p>`;
+    } else if (pw.value === "" || pw.value === null) {
+        error.innerHTML = `<p>Inserisci una password.</p>`;
+    } else if (!validateEmail(email.value)) {
+        error.innerHTML = `<p>inserire una mail valida.</p>`;
+    } else if (pw.value.length < 8) {
+        error.innerHTML = `<p>La password deve avere almeno 8 caratteri.</p>`;
+    } else {
+        login.setAttribute("href", "index.html");
+    }
 }
 const validateEmail = (email) => {
-  return String(email)
-    .toLowerCase()
-    .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
 };
 //
 // pagina todo
 function aggiungiTask(e) {
-  if (taskInput.value === "") {
-    error.innerHTML = `<p>Inserisci un impegno!</p>`;
-  } else {
-    error.innerHTML = ``;
-    taskList.innerHTML += `<li class='rimuovi'>  ${taskInput.value} <i class="bi bi-check-square-fill"></i></li>`;
-    localStorage.setItem("localTask", JSON.stringify(taskList.innerHTML));
-    console.log(localStorage);
-  }
-  taskInput.value = "";
-  e.preventDefault();
+    if (taskInput.value === "") {
+        error.innerHTML = `<p>Inserisci un impegno!</p>`;
+    } else {
+        error.innerHTML = ``;
+        taskList.innerHTML += `<li class='rimuovi'>  ${taskInput.value} <i class="bi bi-check-square-fill"></i></li>`;
+        localStorage.setItem("localTask", JSON.stringify(taskList.innerHTML));
+        console.log(localStorage);
+    }
+    taskInput.value = "";
+    e.preventDefault();
 }
 
 function rimuoviTask(e) {
-  if (e.target.classList.contains("bi")) {
-    e.target.parentElement.remove();
-    localStorage.setItem("localTask", JSON.stringify(taskList.innerHTML));
-    // console.log(localStorage);
-  }
+    if (e.target.classList.contains("bi")) {
+        e.target.parentElement.remove();
+        localStorage.setItem("localTask", JSON.stringify(taskList.innerHTML));
+        // console.log(localStorage);
+    }
 }
 
 // logout
 const logout = document.querySelector(".logout");
 
 function slogga() {
-  localStorage.clear();
+    localStorage.clear();
 }
